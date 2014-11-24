7de2567e1764538f00fb9cccf7a5a03749675bd9
Akira Hayakawa <ruby.wktk@gmail.com>
2014-11-24 17:05:35 +0900

    State.hs in many ways
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- State.hs
+++ State.hs
@@ -1,12 +1,21 @@
 import Control.Monad.State
 
 addUp [] = do
-		a <- get
-		return a
+		-- a <- get
+		-- return a
+		
+		-- gets f = { x <- get; return (f x) }
+		gets id
 
 addUp (x:xs) = do
 		modify (+x) 
 		addUp xs
 
 main = do
+		-- let x = runState (addUp [1,2,3]) 0
+		-- print $ fst x	
+
+		-- print . fst $ runState (addUp [1,2,3]) 0
+
+		-- evalState act = fst . runState act
 		print $ evalState (addUp [1,2,3]) 0
